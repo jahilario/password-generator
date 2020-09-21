1a125dfeb73fe2fddf7a1d0fdd5ccaeeda138ef4
jahilario <josue.a@live.com>
2020-09-20 19:46:38 -0500

    added README file

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,60 @@
+var numbers = "0123456789";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var characeters = "!@#$%^&*+<>?/;:(=)|{~.,";
+var passwordConditions = ["numbers", "lowerCase", "upperCase", "characters"];
+
+
+
+
+
+
 // Assignment code here
+function generatePassword() {
+  var characterPrompt = window.prompt('How many characeters do you want your password to have? Must be at least 8 characters.');
+
+
+
+  if (characterPrompt >= 8 && characterPrompt <=128) {
+   var lowercaseConfirm = window.confirm('Click OK if you want lowercase characters.');
+  }
+
+  else {
+    window.alert("Must be at least 8 character and less than 128 characters.");
+    generatePassword();
+  }
+
+  if (lowercaseConfirm) {
+    var eligibleChars = "";
+    eligibleChars = eligibleChars + lowerCase;
+    var uppercaseConfirm = window.confirm('Click OK if you want uppercase characters.');
+
+  }
+  //If user chooses cancel for uppercare
+
+
+  if (uppercaseConfirm) {
+    var numericConfirm = window.confirm('Click OK if you want numeric characters.');
+  }
+  //If user chooses cancel for numeric
+
+
+  if (numericConfirm) {
+    var specialCharacterConfirm = window.confirm('Click OK if you want special characters.');
+  }
+  //If user chooses cancel for special
+
+  if (specialCharacterConfirm) {
+    window.alert('Enjoy your password!')
+  }
+}
+
+for(var i=0; i <= generatePassword; i++) {
+  password = password + vali
+}
+
+
+
 
 
 // Get references to the #generate element
@@ -12,6 +68,8 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Password Generator Starter Code
+
+![screenshot1](./images/password.JPG) 
+
+## What I Did
+* Webpage looks like the mockup
+* Elements follow a logical structure
+* Presents user with a series of prompts
+* You can choose length of password
+* Decide whether you want lowercase, uppercase, numbers, or special characters
+
+## Tech Used
+* HTML5
+* Javascript
